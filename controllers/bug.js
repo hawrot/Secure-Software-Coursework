e60f243278db35f9650f534fa31a2f906157d3d1
hawrot <mhawro200@caledonian.ac.uk>
2019-11-24 20:48:20 +0000

    Fix the bug displaying data

--- controllers/bug.js
+++ controllers/bug.js
@@ -12,13 +12,12 @@ exports.getAddBug = (req, res, next) =>{
 exports.postAddBug = (req, res, next) =>{
     const name =  req.body.name;
     const desc =  req.body.desc;
-    const date =  req.body.date;
     const assignedTo = req.body.assignedTo;
     const assignedBy = req.body.assignedBy;
     const status = req.body.status;
     const priority = req.body.priority;
 
-    let bug = new Bug(name, desc, date, assignedTo, assignedBy, status, priority);
+    let bug = new Bug(name, desc, assignedTo, assignedBy, status, priority);
     console.log(bug);
     bug.save();
     res.redirect('/view-bug');
